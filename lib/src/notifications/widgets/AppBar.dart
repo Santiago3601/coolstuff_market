5bf9bee817e8fea53e55f044213db720d03d34d5
Andrés Esteban Ávila <66744313+VirtueDevs@users.noreply.github.com>
2023-04-13 21:02:16 -0500

    Color appbar

--- lib/src/notifications/widgets/AppBar.dart
+++ lib/src/notifications/widgets/AppBar.dart
@@ -13,11 +13,14 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       title: Row(
         children: [
-          Icon(Icons.notifications),
+          Icon(Icons.notifications, color: Color.fromARGB(255, 2, 59, 105),),
           SizedBox(width: 8),
-          Text('$notificationCount Notificaciones'),
+          Text('$notificationCount Notificaciones',style: TextStyle(color: Color.fromARGB(255, 2, 59, 105)),),
+          
         ],
       ),
+      backgroundColor: Colors.white,
+      iconTheme: IconThemeData(color: Color.fromARGB(255, 2, 59, 105)),
     );
   }
 }
